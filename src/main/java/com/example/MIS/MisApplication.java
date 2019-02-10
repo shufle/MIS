8fa6d931af1392d889a17075aee08e2f534f2c53
shufle <mikepma36@gmail.com>
2019-02-10 15:15:47 +0300

    Initial commit

---
+++ src/main/java/com/example/MIS/MisApplication.java
@@ -0,0 +1,14 @@
+package com.example.MIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisApplication.class, args);
+	}
+
+}
+
